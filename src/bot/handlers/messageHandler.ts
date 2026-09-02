c37620bf8d3d3ced79290ea3353befc8f0d13a11
Dmitro Adieliev <adelevdmitriy@gmail.com>
2026-09-02 17:50:01 +0100

    Add logs for event handlers

--- src/bot/handlers/messageHandler.ts
+++ src/bot/handlers/messageHandler.ts
@@ -1,7 +1,11 @@
 import { Bot, Context } from 'grammy'
 
 export const handleTextMessages = (bot: Bot) => {
+	console.log('=== Register handle Text Messages ===')
+
 	bot.on('message', (ctx: Context) => {
+		console.log('Bot received a message', JSON.stringify(ctx.message))
+
 		ctx.reply('Your message is', ctx.message)
 	})
 }
--- src/bot/handlers/registerHandlers.ts
+++ src/bot/handlers/registerHandlers.ts
@@ -2,5 +2,7 @@ import { Bot } from 'grammy'
 import { handleTextMessages } from './messageHandler'
 
 export const registerHandlers = (bot: Bot) => {
+	console.log('=== Register event handlers ===')
+
 	handleTextMessages(bot)
 }
