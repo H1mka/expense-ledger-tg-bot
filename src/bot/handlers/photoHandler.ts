b7c81c2c1b38b01a29ee8d1db9d36e15bfe1b877
Dmitro Adieliev <adelevdmitriy@gmail.com>
2026-09-02 19:15:23 +0100

    Remove photo handler debug

--- src/bot/handlers/photoHandler.ts
+++ src/bot/handlers/photoHandler.ts
@@ -3,21 +3,16 @@ import { Bot, Context } from 'grammy'
 export const registerPhotoHandler = (bot: Bot) => {
 	console.log('=== Register photo handler ===')
 
-	bot.on('message:photo', (ctx: Context) => {
+	bot.on('message:photo', async (ctx: Context) => {
 		console.log('Bot received a photo', JSON.stringify(ctx.message))
 
-		try {
-			const photo = ctx.message?.photo?.at(-1)
+		const photo = ctx.message?.photo?.at(-1)
 
-			if (!photo) {
-				ctx.reply('Image not found')
-				return
-			}
-
-			ctx.replyWithPhoto(photo.file_id, { caption: 'This is your photo' })
-			console.log('Bot respond by photo')
-		} catch (error) {
-			console.error('Register photo handler error:', error)
+		if (!photo) {
+			await ctx.reply('Image not found')
+			return
 		}
+
+		await ctx.replyWithPhoto(photo.file_id, { caption: 'This is your photo' })
 	})
 }
