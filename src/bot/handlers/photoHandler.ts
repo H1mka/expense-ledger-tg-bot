dff6353aa09ff6c98157787105563493a565cb97
Dmitro Adieliev <adelevdmitriy@gmail.com>
2026-09-02 19:09:47 +0100

    Separate text and photo handlers

---
+++ src/bot/handlers/photoHandler.ts
@@ -0,0 +1,17 @@
+import { Bot, Context } from 'grammy'
+
+export const registerPhotoHandler = (bot: Bot) => {
+	console.log('=== Register photo handler ===')
+
+	bot.on('message:photo', (ctx: Context) => {
+		console.log('Bot received a photo', JSON.stringify(ctx.message))
+
+		const photo = ctx.message?.photo?.at(-1)
+		if (!photo) {
+			ctx.reply('Image not found')
+			return
+		}
+
+		ctx.replyWithPhoto(photo.file_id, { caption: 'This is your photo' })
+	})
+}
--- src/bot/handlers/registerHandlers.ts
+++ src/bot/handlers/registerHandlers.ts
@@ -1,8 +1,10 @@
 import { Bot } from 'grammy'
 import { handleTextMessages } from './messageHandler'
+import { registerPhotoHandler } from './photoHandler'
 
 export const registerHandlers = (bot: Bot) => {
 	console.log('=== Register event handlers ===')
 
 	handleTextMessages(bot)
+	registerPhotoHandler(bot)
 }
