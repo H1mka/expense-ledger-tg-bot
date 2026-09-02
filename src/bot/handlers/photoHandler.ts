08d4ab524b7162848240e1549d75e4acb1558948
Dmitro Adieliev <adelevdmitriy@gmail.com>
2026-09-02 19:13:25 +0100

    Add debug for register photo handler

--- src/bot/handlers/photoHandler.ts
+++ src/bot/handlers/photoHandler.ts
@@ -6,12 +6,18 @@ export const registerPhotoHandler = (bot: Bot) => {
 	bot.on('message:photo', (ctx: Context) => {
 		console.log('Bot received a photo', JSON.stringify(ctx.message))
 
-		const photo = ctx.message?.photo?.at(-1)
-		if (!photo) {
-			ctx.reply('Image not found')
-			return
-		}
+		try {
+			const photo = ctx.message?.photo?.at(-1)
 
-		ctx.replyWithPhoto(photo.file_id, { caption: 'This is your photo' })
+			if (!photo) {
+				ctx.reply('Image not found')
+				return
+			}
+
+			ctx.replyWithPhoto(photo.file_id, { caption: 'This is your photo' })
+			console.log('Bot respond by photo')
+		} catch (error) {
+			console.error('Register photo handler error:', error)
+		}
 	})
 }
